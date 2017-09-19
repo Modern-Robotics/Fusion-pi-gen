6d06b5361e5431be630308414edd2db5e662051d
Justin Mathews <jmomathews@gmail.com>
2017-09-19 14:04:22 -0400

    new permissions

--- stage4/04-fusion-installs/01-run.sh
+++ stage4/04-fusion-installs/01-run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 
 on_chroot << EOF
+apt-get install --fix-missing
 wget http://raw.githubusercontent.com/Modern-Robotics/Fusion/master/distroInstall.sh
 bash distroInstall.sh
 rm -r distroInstall.sh
