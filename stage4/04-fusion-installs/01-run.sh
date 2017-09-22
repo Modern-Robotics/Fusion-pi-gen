4d9cbea2bc6ec89eef6cd969aac26bcdc1003e1a
Justin Mathews <jmomathews@gmail.com>
2017-09-22 08:56:53 -0400

    added distro install

--- stage4/04-fusion-installs/01-run.sh
+++ stage4/04-fusion-installs/01-run.sh
@@ -2,7 +2,7 @@
 
 on_chroot << EOF
 apt-get install --fix-missing
-wget http://raw.githubusercontent.com/Modern-Robotics/Fusion/master/distroInstall.sh
+wget http://raw.githubusercontent.com/Modern-Robotics/Fusion_pi-gen/master/distroInstall.sh
 bash distroInstall.sh
 rm -r distroInstall.sh
 EOF
