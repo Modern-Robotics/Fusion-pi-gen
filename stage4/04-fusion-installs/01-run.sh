d61719d3f00f1646da38ac015ba4a73f4284fe52
Justin Mathews <jmomathews@gmail.com>
2017-11-15 13:52:08 +0000

    additions

--- stage4/04-fusion-installs/01-run.sh
+++ stage4/04-fusion-installs/01-run.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -e
 
+install -m 777 distroInstall.sh ${ROOTFS_DIR}/
+
 on_chroot << EOF
 apt-get install --fix-missing
-wget http://raw.githubusercontent.com/Modern-Robotics/Fusion/master/distroInstall.sh
-bash distroInstall.sh
+bash /distroInstall.sh
 rm -r distroInstall.sh
 EOF
