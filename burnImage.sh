158083fc8c1ef955caa11bf1f2cc9168563c52c6
Justin Mathews <jmomathews@gmail.com>
2018-02-07 19:08:01 +0000

    stable build process

--- burnImage.sh
+++ burnImage.sh
@@ -1,22 +1,86 @@
 #!/bin/bash
 
+# change to the deploy directory
 cd deploy
 
-# see if /deploy has any .img 
-# if not unzip ...4GB
-
-files=$(ls *4GB.img 2> /dev/null | wc -l)
-if [ **"$files" != "0"** ]
+# Check to see if *4GB.zip has been extracted
+ls *.img
+if [ $? != "0" ]
 then
-#if [ -f '*4GB.img' ] ; then
-    echo "already unzipped"
+    unzip "*4GB.zip"    
 else
-    unzip "*4GB.zip"
+    echo "already unzipped"
 fi
 
-# if sdb* is /dev 
-umount /dev/sdb*
+# Test for which device is the usb key
+# SDA ---------------------------------------------------------------
+udevadm info --query=all --name=sda | grep ID_BUS=usb
+if [ $? == "0" ]
+then
+    echo "USB on device sda, burning image..."
+    umount /dev/sda*
+    image=`ls *.img`
+    dd if=$image of=/dev/sda bs=8192 status=progress &
+else
+    echo "sda is not a usb device"
+fi
 
-# dd if=*/deply/.img*
-image=`ls *.img`
-dd if=$image of=/dev/sdb bs=8192 status=progress
+# SDB ---------------------------------------------------------------
+udevadm info --query=all --name=sdb | grep ID_BUS=usb
+if [ $? == "0" ]
+then
+    echo "USB on device sdb, burning image..."
+    umount /dev/sdb*
+    image=`ls *.img`
+    dd if=$image of=/dev/sdb bs=8192 status=progress &
+else
+    echo "sdb is not a usb device"
+fi
+
+# SDC ---------------------------------------------------------------
+udevadm info --query=all --name=sdc | grep ID_BUS=usb
+if [ $? == "0" ]
+then
+    echo "USB on device sdc, burning image..."
+    umount /dev/sdc*
+    image=`ls *.img`
+    dd if=$image of=/dev/sdc bs=8192 status=progress &
+else
+    echo "sdc is not a usb device"
+fi
+
+# SDD ---------------------------------------------------------------
+udevadm info --query=all --name=sdd | grep ID_BUS=usb
+if [ $? == "0" ]
+then
+    echo "USB on device sdd, burning image..."
+    umount /dev/sdd*
+    image=`ls *.img`
+    dd if=$image of=/dev/sdd bs=8192 status=progress &
+else
+    echo "sdd is not a usb device"
+fi
+
+# SDE ---------------------------------------------------------------
+udevadm info --query=all --name=sde | grep ID_BUS=usb
+if [ $? == "0" ]
+then
+    echo "USB on device sde, burning image..."
+    umount /dev/sde*
+    image=`ls *.img`
+    dd if=$image of=/dev/sde bs=8192 status=progress &
+else
+    echo "sde is not a usb device"
+fi
+
+# SDF ---------------------------------------------------------------
+udevadm info --query=all --name=sdf | grep ID_BUS=usb
+if [ $? == "0" ]
+then
+    echo "USB on device sdf, burning image..."
+    umount /dev/sdf*
+    image=`ls *.img`
+    dd if=$image of=/dev/sdf bs=8192 status=progress &
+else
+    echo "sdf is not a usb device"
+fi
